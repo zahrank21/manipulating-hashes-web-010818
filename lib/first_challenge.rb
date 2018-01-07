c57eeffa58b57a4dcda71654d93585e936625ca3
zahrank21 <zahrank@gmail.com>
2018-01-07 10:06:46 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -19,4 +19,3 @@ contacts["Freddy Mercury"][:favorite_icecream_flavors].reject! {|el| el == "stra
   #remember to return your newly altered contacts hash!
   contacts
 end
-
