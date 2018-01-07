ec9d26f6666ab8486faaa06262d59a0ddddf52c6
zahrank21 <zahrank@gmail.com>
2018-01-07 10:06:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-
+contacts["Freddy Mercury"][:favorite_icecream_flavors].reject! {|el| el == "strawberry"}
 
   #remember to return your newly altered contacts hash!
   contacts
